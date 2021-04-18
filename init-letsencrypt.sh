9e12fbeb658a880b4a6268cd0572be2846c2a2ef
Josh Jacobsen <josh.jacobsen@vitaler.com>
2021-04-18 12:14:33 +1200

    Run for reals

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(test.circle8.eco)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="josh@circle8.eco" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
